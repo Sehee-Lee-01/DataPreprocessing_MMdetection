ae4c5ab362ea3c2ab82d681ec6df2d1ea6b6f7b1
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2022-11-21 16:32:18 +0900

    코드 수정

--- toCoco.py
+++ toCoco.py
@@ -5,15 +5,16 @@ import os
 # 파일 리스트 불러오기, 0: test, 1: train
 name = ["test", "train"]
 originAnnoFilesDir = ["sycData\\test_road_information\\label\\", "sycData\\train_road_information\\label\\"]
+# originAnnoFilesDir = ["sample\\test\\label\\", "sample\\train\\label\\"]
 originAnnoFilesList = [os.listdir(originAnnoFilesDir[0]), os.listdir(originAnnoFilesDir[1])]
  
 # test, train 공통부분
-categoriyDict = {"traffic_sign": 0, "traffic_light": 1, "traffic_information":2}
+categoriyDict = {"traffic_sign": 0, "traffic_light": 1, "traffic_information":2} # 2는 사용 x
 common_categories = [
   {"id": 0, "name": "traffic_sign"},
   {"id": 1, "name": "traffic_light"},
   {"id": 2, "name": "traffic_information"}
-]
+] # 2는 사용 x
  
 for i in range(len(originAnnoFilesList)): # 0: test, 1: train
     CocoDict = {} # coco 데이터셋이 될 딕셔너리
@@ -42,26 +43,27 @@ for i in range(len(originAnnoFilesList)): # 0: test, 1: train
             # 여러개 annotation 정보 하나씩 가공
             for k in range(len(jsonData["annotation"])): # 원본데이터 안에서 하나의 annotation 정보 처리
                 categoryId = categoriyDict[jsonData["annotation"][k]["class"]]
-                width = jsonData["annotation"][k]["box"][2]-jsonData["annotation"][k]["box"][0]
-                height = jsonData["annotation"][k]["box"][3]-jsonData["annotation"][k]["box"][1]
-                area = width*height
-                bbox = [
-                    jsonData["annotation"][k]["box"][0],
-                    jsonData["annotation"][k]["box"][1],
-                    width,
-                    height
-                ]
-                annotation = {
-                    "segmentation": [[]],
-                    "image_id": imgId,
-                    "bbox": bbox,
-                    "category_id": categoryId,
-                    "id": annotationCount,  
-                    "area": area,
-                    "iscrowd": 0,
-                }
-                cocoAnnos.append(annotation)
-                annotationCount += 1
+                if categoryId != 2:
+                    width = jsonData["annotation"][k]["box"][2]-jsonData["annotation"][k]["box"][0]
+                    height = jsonData["annotation"][k]["box"][3]-jsonData["annotation"][k]["box"][1]
+                    area = width*height
+                    bbox = [
+                        jsonData["annotation"][k]["box"][0],
+                        jsonData["annotation"][k]["box"][1],
+                        width,
+                        height
+                    ]
+                    annotation = {
+                        "segmentation": [[]],
+                        "image_id": imgId,
+                        "bbox": bbox,
+                        "category_id": categoryId,
+                        "id": annotationCount,  
+                        "area": area,
+                        "iscrowd": 0,
+                    }
+                    cocoAnnos.append(annotation)
+                    annotationCount += 1
  
     CocoDict["images"] = cocoImgs # coco 데이터 images 부분
     CocoDict["annotations"] = cocoAnnos # coco 데이터 annotations 부분
